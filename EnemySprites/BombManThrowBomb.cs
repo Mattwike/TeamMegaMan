5e9bbf6bf975477ec57f6452565b3eecd1e873eb
Tburk18 <92115744+Tburk18@users.noreply.github.com>
2024-09-30 19:12:54 -0400

    small changes to animation on boss
    also trying to resolve merge conflicts

--- EnemySprites/BombManThrowBomb.cs
+++ EnemySprites/BombManThrowBomb.cs
@@ -12,23 +12,41 @@ public class BombManThrowBomb : IEnemySprite
     private Texture2D enemySheet;
     int enemySizeX;
     int enemySizeY;
-    public int bombInAir = 0;
+
+    float bombX;
+    float bombY;
+    public int bombInAir;
+    int bombSizeX;
+    int bombSizeY;
+    private float gravity = 1f;
+    private float bombSpeed = 3f;
 
     public BombManThrowBomb(Texture2D texture)
     {
         enemySheet = texture;
         x = 450;
         y = 50;
+
+     
+
+
     }
 
     public void Initialize(GraphicsDeviceManager _graphics, float movementSpeed, int megamanSize)
     {
         currentFrame = 0;
-        totalFrame = 60;
+        totalFrame = 70;
         delayCounter = 0;
         delayMax = 10;
         enemySizeX = 50;
         enemySizeY = 50;
+
+        bombSizeX = 25;
+        bombSizeY = 25;
+
+        bombX = 470;
+        bombY = 50;
+
     }
 
     public void Update(GameTime gameTime)
@@ -36,6 +54,11 @@ public class BombManThrowBomb : IEnemySprite
         delayCounter++;
         if (delayCounter >= delayMax)
         {
+            if (bombInAir == 1)
+            {
+                bombX += bombSpeed;
+                bombY += gravity;
+            }
             currentFrame++;
             if (currentFrame >= totalFrame)
             {
@@ -43,6 +66,8 @@ public class BombManThrowBomb : IEnemySprite
             }
             delayCounter = 0;
         }
+    
+       
     }
 
 
@@ -62,30 +87,41 @@ public class BombManThrowBomb : IEnemySprite
             spriteEffects |= SpriteEffects.FlipVertically;
         }
 
-        Rectangle sourceRectangle;
-        Rectangle destinationRectangle;
-        Rectangle sourceRectangleBomb;
-        Rectangle destinationRectangleBomb;
+        Rectangle sourceRectangle = Rectangle.Empty;
+        Rectangle destinationRectangle = Rectangle.Empty;
+        Rectangle sourceRectangleBomb = Rectangle.Empty;
+        Rectangle destinationRectangleBomb = Rectangle.Empty;
 
         if(currentFrame < 20)
         {
             sourceRectangle = new Rectangle(195, 29, 33, 34);
             destinationRectangle = new Rectangle((int)x, (int)y, enemySizeX, enemySizeY);
-        } else
+        } else if (currentFrame >= 20 && currentFrame < 22)
         {
-            sourceRectangle = new Rectangle(218, 61, 37, 30);
-            destinationRectangle = new Rectangle((int)x, (int)y, enemySizeX, enemySizeY);
+            sourceRectangle = new Rectangle(218, 63, 33, 25);
+            destinationRectangle = new Rectangle((int)x, (int)y+9, enemySizeX, enemySizeY-9);
+            bombInAir = 1;
+        } else if(currentFrame >= 22 && currentFrame <= 70)
+        {
+            //bombMan enemy
+            sourceRectangle = new Rectangle(218, 63, 33, 25);
+            destinationRectangle = new Rectangle((int)x, (int)y + 9, enemySizeX, enemySizeY - 9);
 
-           
-        }
-        sourceRectangleBomb = new Rectangle(0, 123, 16, 18);
-        destinationRectangleBomb = new Rectangle((int)x + 100, (int)y, enemySizeX, enemySizeY);
-
-
+            //Bomb arc
+            sourceRectangleBomb = new Rectangle(0, 123, 16, 18);
+            destinationRectangleBomb = new Rectangle((int)bombX, (int)bombY, bombSizeX, bombSizeY);
+        } 
+       
 
         
         _spriteBatch.Begin();
-        _spriteBatch.Draw(enemySheet, destinationRectangleBomb, sourceRectangleBomb, Color.White, 0f, Vector2.Zero, spriteEffects, 0f);
+
+        if(bombInAir == 1)
+        {
+            _spriteBatch.Draw(enemySheet, destinationRectangleBomb, sourceRectangleBomb, Color.White, 0f, Vector2.Zero, spriteEffects, 0f);
+
+        }
+
         _spriteBatch.Draw(enemySheet, destinationRectangle, sourceRectangle, Color.White, 0f, Vector2.Zero, spriteEffects, 0f);
         _spriteBatch.End();
 
