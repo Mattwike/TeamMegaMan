947232a7dab97fc618c800470cb576df481903ae
snallapati1 <shivaramnallapati@gmail.com>
2024-09-30 21:42:42 -0400

    finished the octopus enemy

--- SpriteFactories/EnemySpriteFactory.cs
+++ SpriteFactories/EnemySpriteFactory.cs
@@ -45,4 +45,9 @@ public class EnemySpriteFactory
     {
         return new BombManThrowBomb(bossSheet);
     }
+
+    public IEnemySprite CreateOctopus()
+    {
+        return new Octopus(enemySheet);
+    }
 }
