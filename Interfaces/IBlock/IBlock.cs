3f9ca0ec8a8faf6136032996b252d79441529c9b
Tburk18 <92115744+Tburk18@users.noreply.github.com>
2024-10-16 19:39:47 -0400

    added floor object, block sprite machine
    no collision for blocks yet, and just the 3 basic floor blocks implemented

---
+++ Interfaces/IBlock/IBlock.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public interface IBlock
+{
+    public void Update();
+
+    public void Initialize();
+
+    public void Draw(SpriteBatch spriteBatch, int blockNum, int blockWidth, Vector2 pos);
+
+}
---
+++ SpriteFactories/BlockSpriteFactory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class BlockSpriteFactory
+{
+    private static BlockSpriteFactory instance = new BlockSpriteFactory();
+    private Texture2D blockSheet;
+
+    public static BlockSpriteFactory Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private BlockSpriteFactory() { }
+
+    public void LoadAllTextures(ContentManager content)
+    {
+        blockSheet = content.Load<Texture2D>("megamanBlocks");
+
+    }
+
+    public IBlock CreateFloorEndLeft()
+    {
+        return new FloorEndLeft(blockSheet);
+    }
+    public IBlock CreateFloorEndRight()
+    {
+        return new FloorEndRight(blockSheet);
+    }
+
+    public IBlock CreateFloorMiddle()
+    {
+        return new FloorMiddle(blockSheet);
+    }
+
+ 
+
+
+}
