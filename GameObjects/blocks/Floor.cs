b8c51b79e5876bf938a9b72ca90140a1e7f64e87
Matthew Weikel <matthewdweikel@gmail.com>
2024-10-20 22:28:09 -0400

    added file missing from last commit

--- GameObjects/blocks/Floor.cs
+++ GameObjects/blocks/Floor.cs
@@ -1,62 +1,18 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using System;
-using System.Collections.Generic;
+using Project1.Interfaces;
+using Project1.States.MegamanState;
+using Microsoft.Xna.Framework.Input;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-public class Floor
+
+
+namespace Project1.GameObjects
 {
-    //Floor modeled as a list of blocks arranged in a straight line.
-    private int numBlocks;
-    private Vector2 pos;
-    public Rectangle boundingBox;
 
-    int blockWidth = 16;
-    int blockHeight = 16;
-
-    List<IBlock> blocksInFloorSegment;
-
-    public Floor(int numOfBlocks, Vector2 startPos)
+    public class Floor : IBlocks
     {
-        this.numBlocks = numOfBlocks;
-        pos = startPos;
-
-        blocksInFloorSegment = new List<IBlock>(numOfBlocks);
-        for (int i = 0; i < numOfBlocks; i++)
-        {
-            blocksInFloorSegment.Add(BlockSpriteFactory.Instance.CreateFloorMiddle());
-        }
-
-
-        if (blocksInFloorSegment.Count > 0)
-        {
-
-            blocksInFloorSegment[0] = BlockSpriteFactory.Instance.CreateFloorEndLeft();
-            for (int i = 1; i < numOfBlocks - 1; i++)
-            {
-                blocksInFloorSegment[i] = BlockSpriteFactory.Instance.CreateFloorMiddle();
-            }
-            blocksInFloorSegment[numOfBlocks - 1] = BlockSpriteFactory.Instance.CreateFloorEndRight();
-
-            boundingBox = new Rectangle((int)pos.X, (int)pos.Y, blockWidth * numOfBlocks, blockHeight);
-        }
-
+        public Rectangle FloorBox = new Rectangle(0, 200, 100, 10);
     }
-
-    public void Draw(SpriteBatch spriteBatch)
-    {
-        int blockNum = 0;
-        foreach (IBlock block in blocksInFloorSegment)
-        {
-            block.Draw(spriteBatch, blockNum, blockWidth, pos);
-            blockNum++;
-        }
-       
-        
-
-    }
-
 
 }
