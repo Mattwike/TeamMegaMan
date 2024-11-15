e535eacdb974782a7c12b6e2c392e99a6d204f0a
Tburk18 <92115744+Tburk18@users.noreply.github.com>
2024-11-14 21:34:37 -0500

    added zoom function for camera

--- GameControlers/Camera.cs
+++ GameControlers/Camera.cs
@@ -6,18 +6,38 @@ public class Camera
 {
     private Viewport view;
     public Vector2 Position { get; set; }
+    private float zoom;
+    
 
     public Camera(Viewport port)
     {
         view = port;
         Position = Vector2.Zero;
+        zoom = 1.2f;
+
+
         
     }
 
     public Matrix GetTransform()
     {
-        
-        return Matrix.CreateTranslation(-Position.X + (view.Width / 2), -Position.Y + (view.Height / 2), 0f);
+        Vector2 newPosition = Position - (new Vector2(view.Width, view.Height - 300*zoom) * 0.5f / zoom);
+
+        Matrix positionMatrix = Matrix.CreateTranslation(-newPosition.X, -newPosition.Y, 0f);
+        Matrix zoomMatrix = Matrix.CreateScale(zoom, zoom, 1f);
+
+        return positionMatrix * zoomMatrix;
     }
+
+    public void Zoom(float newZoom)
+    {
+        zoom = newZoom;
+        if(zoom < 0.1f)
+        {
+            zoom = 0.1f;
+        }
+    }
+
+ 
 
 }
